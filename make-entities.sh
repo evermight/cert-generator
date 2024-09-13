1d03316adc8b58db2c5536c631ac3faa7f9060ef
root <root@provisioner.hitronhub.home>
2024-09-13 14:07:21 +0000

    making certs

---
+++ make-entities.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
+cd $script_dir;
+
+
+fqdns=("node1.evermight.net" "node2.evermight.net")
+
+for index in "${!fqdns[@]}"
+do
+
+run=$(($index + 1))
+
+mkdir -p "${fqdns[$index]}";
+
+cd "${fqdns[$index]}";
+
+cp ../meta.cnf ./;
+
+sed -i 's/##FQDN##/'"${fqdns[$index]}"'/g' meta.cnf
+
+openssl genrsa -out entity.key 2048
+
+openssl req -new -sha256 -nodes -key entity.key -config meta.cnf -out entity.csr
+
+openssl x509 -req -in entity.csr -CA ../ca/root.crt -CAkey ../ca/root.key \
+  -CAcreateserial -out entity.crt -days 500 -sha256 -extensions v3_req \
+  -extfile meta.cnf;
+
+cat entity.crt ../ca/root.crt > entity.full.crt;
+cd ../
+
+done
